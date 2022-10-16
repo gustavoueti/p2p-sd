79aef9a52b38ca9b28ebf49b4a0f0798e2e859bb
Gabriel Agostini <agostini.gabriel@Air-de-Gabriel.local>
2022-10-16 00:33:52 -0300

    Add message class

---
+++ src/p2p/Mensagem.java
@@ -0,0 +1,79 @@
+package p2p;
+
+public class Mensagem {
+	public static enum MessageType {
+		SEARCH(1), RESPONSE(2);	
+		private final int value;
+		
+		MessageType(int selectedValue){
+			value = selectedValue;
+		}
+		
+		public int getValue(){
+			return value;
+		}
+	}
+	
+	public static enum Responses {
+		NOT_FOUND(1),FOUND(2);
+		
+		private final int value;
+		
+		Responses(int selectedValue){
+			value = selectedValue;
+		}
+		
+		public int getValue(){
+			return value;
+		}
+		
+	}
+	
+	private String messageContent;
+	private String reqId;
+	private MessageType reqType;
+	private Responses res;
+	
+	public String getMessageContent() {
+		return messageContent;
+	}
+
+	public void setMessageContent(String messageContent) {
+		this.messageContent = messageContent;
+	}
+
+	public String getReqId() {
+		return reqId;
+	}
+
+	public void setReqId(String reqId) {
+		this.reqId = reqId;
+	}
+
+	public MessageType getReqType() {
+		return reqType;
+	}
+
+	public void setReqType(MessageType reqType) {
+		this.reqType = reqType;
+	}
+
+	public Responses getRes() {
+		return res;
+	}
+
+	public void setRes(Responses res) {
+		this.res = res;
+	}
+	
+	public Mensagem(MessageType type, String content, String reqId) {
+		this.messageContent = content;
+		this.reqId = reqId;
+		this.reqType = type;
+		
+	}
+	
+	public Mensagem() {
+		
+	}
+}
