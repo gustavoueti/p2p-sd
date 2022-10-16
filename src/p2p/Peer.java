6fc55520cf6c4e4a78854ce47e213dbe11163d4f
Gabriel Agostini <agostini.gabriel@MacBook-Air-de-Gabriel.local>
2022-10-16 15:58:08 -0300

    Add Directory Monitor

--- src/p2p/Peer.java
+++ src/p2p/Peer.java
@@ -1,5 +1,8 @@
 package p2p;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Peer {
@@ -24,23 +27,24 @@ public class Peer {
 	}
 	
 	public static class ListenThread extends Thread {
-		public ListenThread(String threadId) {
-			
-		}
-		
 		public void run() {
 			
 		}
 	}
 	
 	public static class MenuThread extends Thread {
+		private MonitorThread fileMonitor;
+		private List<String> peersIPList = new ArrayList<String>();
+		private String peerId, pathName;
+		private Scanner input = new Scanner(System.in);
+		
 		public void run() {
 			printMenu();
-			Scanner in = new Scanner(System.in);
 			while(true) {
-			    int anwser = in.nextInt();
+			    int anwser = Integer.valueOf(this.input.nextLine());
 			    if(anwser == ConsoleOption.INICIALIZA.getValue()) {
-			    	
+			    	this.requestInitVariables();
+			    	this.initMonitor();
 			    } else if(anwser == ConsoleOption.SEARCH.getValue()) {
 			    	
 			    } else {
@@ -49,20 +53,59 @@ public class Peer {
 			    }
 			}
 		}
-		public static void printMenu() {
+		
+		private static void printMenu() {
 			System.out.println("Peer Menu - Pressione:");
 			System.out.println(ConsoleOption.INICIALIZA.getValue() + "-" + ConsoleOption.INICIALIZA);
 			System.out.println(ConsoleOption.SEARCH.getValue() + "-" + ConsoleOption.SEARCH);
 		}
+		
+		private void requestInitVariables() {
+			int nPeers;
+			System.out.println("Inicializando peer - Digite o IP do peer:");
+			this.peerId = this.input.nextLine();
+			System.out.println("Digite o número de peers");
+			nPeers = Integer.valueOf(this.input.nextLine());
+			for(int i=0;i<nPeers;i++) {
+				System.out.println("Digite o IP do peer " + i + ":");
+				String peerIP = this.input.nextLine();
+				this.peersIPList.add(peerIP);
+			}
+			System.out.println("Qual o diretorio deste peer:");
+			this.pathName = this.input.nextLine();
+		}
+		
+		private void initMonitor() {
+			this.fileMonitor = new MonitorThread(this.peerId,this.pathName);
+			this.fileMonitor.start();
+		}
 	}
 	
 	public static class MonitorThread extends Thread {
+		private String pathName, peerId;
+		private String[] files;
+		
 		public MonitorThread(String peerId,String pathName) {
-			
+			this.peerId = peerId;
+			this.pathName = pathName;
 		}
 		
 		public void run() {
-			
+			File dir = new File(this.pathName);
+			String concatNames;
+			while(true) {
+				this.files = dir.list();
+				concatNames = "";
+				for(String fileName: this.files) {
+					concatNames = concatNames + " " + fileName;
+				}
+				System.out.println("Sou peer "+  this.peerId + " com arquivos" + concatNames);
+				try {
+					Thread.sleep(30000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
 		}
 	}
 	
